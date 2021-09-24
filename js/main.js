60f257cc8f48f602613b9062bc032e26f0f9ea92
iftixor20 <iorolov5@gmail.com>
2021-09-24 07:44:01 -0700

    coomit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-var NEW_ROW = document.getElementById('input');
-var numerator = [];
+var card = document.getElementById('input');
 
 for(var i = 0; i < pokemons.length; i++) {
 
     var NEW_COL = document.createElement('li');
     NEW_COL.classList.add('col-3', 'mb-4');
-    NEW_ROW.appendChild(NEW_COL);
+    card.appendChild(NEW_COL);
 
     var NEW_CARD = document.createElement('div');
     NEW_CARD.classList.add('card');
@@ -25,18 +24,23 @@ for(var i = 0; i < pokemons.length; i++) {
     NEW_DIV.classList.add('card-body');
     NEW_CARD.appendChild(NEW_DIV);
 
-    var POCEMON_NAME = document.createElement('p');
-    POCEMON_NAME.classList.add('card-title', 'fs-6', 'fw-bold', 'text-center', 'text-truncate');
+    var POCEMON_NAME = document.createElement('h1');
+    POCEMON_NAME.classList.add('card-title', 'fs-4', 'fw-bold', 'text-center', 'text-truncate');
     POCEMON_NAME.textContent = 'Name: ' + pokemons[i].name;
     NEW_DIV.appendChild(POCEMON_NAME);
 
     var POCEMON_TYPE = document.createElement('p');
-    POCEMON_TYPE.classList.add('card-text', 'fs-6', 'fw-normal', 'text-center', 'text-liner', 'fst-italic');
+    POCEMON_TYPE.classList.add('card-text', 'fs-6', 'fw-normal', 'text-center', 'text-liner');
     POCEMON_TYPE.textContent = 'Type: ' + pokemons[i].type;
     NEW_DIV.appendChild(POCEMON_TYPE);
 
-    var num = document.createElement('p');
+    var num = document.createElement('span');
     num.classList.add('card-text', 'fs-6', 'fw-normal', 'text-center', 'text-liner');
     num.textContent = 'Num: ' + pokemons[i].num;
     NEW_DIV.appendChild(num);
+
+    var time = document.createElement('span');
+    time.classList.add('card-text', 'fs-6', 'fw-normal', 'text-center', 'm-5', 'text-liner');
+    time.textContent = 'Time: ' + pokemons[i].spawn_time;
+    NEW_DIV.appendChild(time);
 }
