08f3f042d025eb8dd48f35e2a5dc38554d5bfe66
iftixor20 <iorolov5@gmail.com>
2021-09-24 07:51:26 -0700

    coomit

--- js/main.js
+++ js/main.js
@@ -2,27 +2,27 @@ var card = document.getElementById('input');
 
 for(var i = 0; i < pokemons.length; i++) {
 
-    var NEW_COL = document.createElement('li');
-    NEW_COL.classList.add('col-3', 'mb-4');
-    card.appendChild(NEW_COL);
+    var coll = document.createElement('li');
+    coll.classList.add('col-3', 'mb-4');
+    card.appendChild(coll);
 
-    var NEW_CARD = document.createElement('div');
-    NEW_CARD.classList.add('card');
-    NEW_COL.appendChild(NEW_CARD);
+    var cards = document.createElement('div');
+    cards.classList.add('card');
+    coll.appendChild(cards);
 
-    var CARD_TOP = document.createElement('div');
-    CARD_TOP.classList.add('card-img-top', 'd-flex', 'justify-content-center');
-    NEW_CARD.appendChild(CARD_TOP);
+    var cards_top = document.createElement('div');
+    cards_top.classList.add('card-img-top', 'd-flex', 'justify-content-center');
+    cards.appendChild(cards_top);
 
-    var NEW_IMG = document.createElement('img');
-    NEW_IMG.setAttribute('src', pokemons[i].img)
-    NEW_IMG.setAttribute('width', '130')
-    NEW_IMG.setAttribute('height', '130')
-    CARD_TOP.appendChild(NEW_IMG);
+    var imgage = document.createElement('img');
+    imgage.setAttribute('src', pokemons[i].img)
+    imgage.setAttribute('width', '130')
+    imgage.setAttribute('height', '130')
+    cards_top.appendChild(imgage);
 
     var NEW_DIV = document.createElement('div');
     NEW_DIV.classList.add('card-body');
-    NEW_CARD.appendChild(NEW_DIV);
+    cards.appendChild(NEW_DIV);
 
     var POCEMON_NAME = document.createElement('h1');
     POCEMON_NAME.classList.add('card-title', 'fs-4', 'fw-bold', 'text-center', 'text-truncate');
